db3f685f8295e65be3c8541aff26f4fd873ed88c
Gerardo Santillana <Gerardo Santillana>
2016-07-14 18:58:16 -0400

    Add search text function with error

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -26,5 +26,35 @@ module.exports = {
     // }
     // this code is the same as the above
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // filter by showCompleted
+    // .filter is a built-in array method,  take an existin array
+    // required a callback function, the callback is called one time for
+    // every item in the array, the array is todos the element is todo
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // // filter by searchText
+    //  filteredTodos = filteredTodos.filter((todo) => {
+    //     var text = todo.text.toLowerCase;
+    //    return searchText.length === 0 || text.indexOf(searchText) > -1;
+    //  });
+
+    // sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed  && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
