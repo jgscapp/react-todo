3706af8087cbec43312d0d6d9487bbc9a40f3e7f
Gerardo Santillana <Gerardo Santillana>
2016-08-20 00:16:59 -0400

    Run webpack on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'develpment';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
