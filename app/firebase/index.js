058abd4d00850a7fa5ee6282f89d0088930b7663
Gerardo Santillana <Gerardo Santillana>
2016-08-24 22:42:34 -0400

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 try {
   var config = {
     apiKey: process.env.API_KEY,
-    authDomain: process_env.AUTH_DOMAIN,
+    authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
     storageBucket: process.env.STORAGE_BUCKET,
   };
