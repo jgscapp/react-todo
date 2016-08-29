168cddf5a0d17b4095233d546aa31f70fcd04449
Gerardo Santillana <Gerardo Santillana>
2016-08-29 00:27:37 -0400

    Update todos location to include uid

--- app/app.jsx
+++ app/app.jsx
@@ -11,6 +11,8 @@ import router from 'app/router/';
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(actions.login(user.uid));
+    //asynchronus action goes off fetch data from firebase
+    store.dispatch(actions.startAddTodos());
     hashHistory.push('/todos');
   } else {
     store.dispatch(actions.logout());
@@ -31,9 +33,6 @@ firebase.auth().onAuthStateChanged((user) => {
 // var initialTodos = TodoAPI.getTodos();
 // store.dispatch(actions.addTodos(initialTodos));
 
-
-//asynchronus action goes off fetch data from firebase
-store.dispatch(actions.startAddTodos());
 
 
 //default values
